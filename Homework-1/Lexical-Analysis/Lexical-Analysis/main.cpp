972ff675a9facb27900df3eab247eb3b0974dc53
Feng-Yz <1150125812@qq.com>
2021-09-18 15:02:59 +0800

    Update main.cpp

--- Homework-1/Lexical-Analysis/Lexical-Analysis/main.cpp
+++ Homework-1/Lexical-Analysis/Lexical-Analysis/main.cpp
@@ -14,6 +14,16 @@
 
 using namespace std;
 
+bool isSeparator(char ch)
+{
+    if (ch == ' ' || ch == ',' || ch == ';' || ch == '(' || ch == ')' ||
+        ch == '[' || ch == ']' || ch == '{' || ch == '}' || ch == '\t' || ch == '\n')
+    {
+        return true;
+    }
+    return false;
+}
+
 bool isPunctuator(char ch) // check if the given character is a punctuator or not
 {
     if (ch == ' ' || ch == '+' || ch == '-' || ch == '*' ||
@@ -27,38 +37,28 @@ bool isPunctuator(char ch) // check if the given character is a punctuator or no
     return false;
 }
 
-bool validIdentifier(char* str)                        //check if the given identifier is valid or not
+bool validIdentifier(char* str) // check if the given identifier is valid or not
 {
-    if (str[0] == '0' || str[0] == '1' || str[0] == '2' ||
-        str[0] == '3' || str[0] == '4' || str[0] == '5' ||
-        str[0] == '6' || str[0] == '7' || str[0] == '8' ||
-        str[0] == '9' || isPunctuator(str[0]) == true)
-        {
-            return false;
-        }                                    //if first character of string is a digit or a special character, identifier is not valid
-    int i,len = strlen(str);
-    if (len == 1)
-    {
-        return true;
-    }                                        //if length is one, validation is already completed, hence return true
-    else
-    {
-    for (i = 1 ; i < len ; i++)                        //identifier cannot contain special characters
-    {
-        if (isPunctuator(str[i]) == true)
-        {
+    if (!(str[0] == '_' || (str[0] <= 'z' && str[0] >= 'a') || (str[0] <= 'Z' && str[0] >= 'A'))) {
+        return false;
+    }
+    for (int i = 1; i < strlen(str); i++) {
+        if (!(str[i] == '_'
+              || (str[i] <= 'z' && str[i] >= 'a')
+              || (str[i] <= 'Z' && str[i] >= 'A')
+              || (str[i] <= '9' && str[i] >= '0'))) {
             return false;
         }
-    }
     }
     return true;
 }
 
 bool isOperator(char ch) // check if the given character is an operator or not
 {
-    if (ch == '+' || ch == '-' || ch == '*' ||
-        ch == '/' || ch == '>' || ch == '<' ||
-        ch == '=' || ch == '|' || ch == '&')
+    if (ch == '+' || ch == '-' || ch == '*' || ch == '%' ||
+        ch == '/' || ch == '>' || ch == '<' || ch == '!' ||
+        ch == '=' || ch == '|' || ch == '&' || ch == '^' ||
+        ch == '~' || ch == '?' || ch == ':')
     {
        return true;
     }
@@ -67,21 +67,15 @@ bool isOperator(char ch) // check if the given character is an operator or not
 
 bool isKeyword(char *str) // check if the given substring is a keyword or not
 {
-    if (!strcmp(str, "if") || !strcmp(str, "else") ||
-        !strcmp(str, "while") || !strcmp(str, "do") ||
-        !strcmp(str, "break") ||  !strcmp(str, "continue")
-        || !strcmp(str, "int") || !strcmp(str, "double")
-        || !strcmp(str, "float") || !strcmp(str, "return")
-        || !strcmp(str, "char") || !strcmp(str, "case")
-        || !strcmp(str, "long") || !strcmp(str, "short")
-        || !strcmp(str, "typedef") || !strcmp(str, "switch")
-        || !strcmp(str, "unsigned") || !strcmp(str, "void")
-        || !strcmp(str, "static") || !strcmp(str, "struct")
-        || !strcmp(str, "sizeof") || !strcmp(str,"long")
-        || !strcmp(str, "volatile") || !strcmp(str, "typedef")
-        || !strcmp(str, "enum") || !strcmp(str, "const")
-        || !strcmp(str, "union") || !strcmp(str, "extern")
-        || !strcmp(str,"bool"))
+    if (!strcmp(str, "if") || !strcmp(str, "else") || !strcmp(str, "while") || !strcmp(str, "do")
+        || !strcmp(str, "break") ||  !strcmp(str, "continue") || !strcmp(str, "int") || !strcmp(str, "double")
+        || !strcmp(str, "float") || !strcmp(str, "return") || !strcmp(str, "char") || !strcmp(str, "case")
+        || !strcmp(str, "long") || !strcmp(str, "short") || !strcmp(str, "signed") || !strcmp(str, "switch")
+        || !strcmp(str, "unsigned") || !strcmp(str, "void") || !strcmp(str, "static") || !strcmp(str, "struct")
+        || !strcmp(str, "sizeof") || !strcmp(str,"default") || !strcmp(str, "volatile") || !strcmp(str, "typedef")
+        || !strcmp(str, "enum") || !strcmp(str, "const") || !strcmp(str, "union") || !strcmp(str, "extern")
+        || !strcmp(str, "bool") || !strcmp(str, "for") || !strcmp(str, "goto") || !strcmp(str, "register")
+        || !strcmp(str, "auto"))
     {
         return true;
     }
@@ -118,7 +112,7 @@ bool isNumber(char* str) // check if the given substring is a number or not
     return true;
 }
 
-char* subString(char* realStr, int l, int r) // extract the required substring from the main string
+char* subString(string realStr, int l, int r) // extract the required substring from the main string
 {
     char* str = (char*) malloc(sizeof(char) * (r - l + 2));
     for (int i = l; i <= r; i++)
@@ -130,31 +124,63 @@ char* subString(char* realStr, int l, int r) // extract the required substring f
 }
 
 
-void lexicalAnalyse(char* str)
+void lexicalAnalyse(string str)
 {
     int left = 0, right = 0;
-    unsigned long len = strlen(str);
+    unsigned long len = str.size();
     while (right <= len && left <= right) {
-        if (isPunctuator(str[right]) == false) // if character is a digit or an alphabet
-        {
-            right++;
-            if (right > len)
-            {
-                break;
+        // identify the string
+        if (str[left] == '"') {
+            int i = left + 1;
+            for ( ; i < len; i++) {
+                if (str[i] == '"') {
+                    break;
+                }
             }
-        }
-
-        if (isPunctuator(str[right]) == true && left == right) // if character is a punctuator
-        {
-            if (isOperator(str[right]) == true)
-            {
-                std::cout<< str[right] <<" IS AN OPERATOR\n";
-            }
-            right++;
+            char* sub = subString(str, left, i);
+            cout << sub << " IS A STRING\n";
+            right = i + 1;
             left = right;
         }
-        else if ((isPunctuator(str[right]) == true && left != right)
-                 || (right == len && left != right)) // check if parsed substring is a keyword or identifier or number
+        
+        // identify the character
+        if (str[left] == '\'') {
+            int i = left + 1;
+            for ( ; i < len; i++) {
+                if (str[i] == '\'') {
+                    break;
+                }
+            }
+            char* sub = subString(str, left, i);
+            cout << sub << " IS A CHARACTER\n";
+            right = i + 1;
+            left = right;
+        }
+        
+        // identify the preprocessor statement
+        if (str[left] == '#')
+        {
+            cout << str << " IS A PREPROCESSOR STATEMENT";
+            break;
+        }
+        
+        if (isSeparator(str[left]) == true && left == right) {
+            left ++;
+            right = left;
+        }
+        else if (isOperator(str[left]) == true && left == right) {
+            int i = left + 1;
+            for ( ; i < len; i++) {
+                if (isOperator(str[i]) == false) {
+                    break;
+                }
+            }
+            char* sub = subString(str, left, i - 1);
+            cout << sub << " IS A OPERATOR\n";
+            right = i;
+            left = right;
+        }
+        else if ((isSeparator(str[right]) || isOperator(str[right]) || right == len) && left != right)
         {
             char* sub = subString(str, left, right - 1); // extract substring
             if (isKeyword(sub) == true)
@@ -165,17 +191,19 @@ void lexicalAnalyse(char* str)
             {
                 cout<< sub <<" IS A NUMBER\n";
             }
-            else if (validIdentifier(sub) == true
-                     && isPunctuator(str[right - 1]) == false)
+            else if (validIdentifier(sub) == true)
             {
                 cout<< sub <<" IS A VALID IDENTIFIER\n";
             }
-            else if (validIdentifier(sub) == false
-                     && isPunctuator(str[right - 1]) == false)
+            else if (validIdentifier(sub) == false)
             {
                 cout<< sub <<" IS NOT A VALID IDENTIFIER\n";
             }
             left = right;
+        }
+        
+        if (isSeparator(str[right]) == false && isOperator(str[right]) == false && right <= len) {
+            right ++;
         }
     }
     return;
@@ -185,12 +213,10 @@ int main(int argc, const char * argv[]) {
     using namespace std;
     ifstream infile;
     infile.open("/Users/fxb/Desktop/大三上/Java语言程序设计/Homework-1/Lexical-Analysis/Lexical-Analysis/test.c");
-
-    char buf[1024];
-    while(infile.getline(buf, sizeof(buf)))
-    {
-        cout<<buf<<std::endl;
-        lexicalAnalyse(buf);
+    string line;
+    while (getline(infile, line)) {
+        cout << line << endl;
+        lexicalAnalyse(line);
         cout << endl;
     }
     infile.close();
