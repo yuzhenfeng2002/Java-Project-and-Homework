8ffc4ee223b35c3c18e4718af767c770b8453e1b
Feng-Yz <1150125812@qq.com>
2021-12-18 19:15:39 +0800

    大作业说明书

--- FinalProject/Exercise-Web-Online/src/test/java/com/Yuzhen/ExerciseOnline/ExerciseOnlineApplicationTest.java
+++ FinalProject/Exercise-Web-Online/src/test/java/com/Yuzhen/ExerciseOnline/ExerciseOnlineApplicationTest.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ExerciseOnlineApplicationTest {
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 }
